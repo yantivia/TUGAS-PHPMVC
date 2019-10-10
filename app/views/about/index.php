c44b5a25486eae6c78ba92ff1725cedc7e3f1d0b
Yantivia R. Yumame <yantivia24@gmail.com>
2019-10-10 22:46:08 +0700

    membuat view untuk antar muka aplikasi

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -2,5 +2,5 @@
 	<h1 class ="mt-4">About me</h1>
 	<img src="<?= BASEURL; ?>/img/yanti.JPG" alt="Yantivia Yumame" width="200" class="rounded-circle shadow">
 	<p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorng <?= $data['pekerjaan']; ?> </p>
-
+	
 </div>
